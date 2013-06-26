06f262f1c4209769179fc24168f03c605d422dd9
Murilo F. M <muhrix@gmail.com>
2013-06-26 20:55:01 -0300

    Updated fake data publisher to work properly with Altimeter and Variometer

--- ground_station/src/fake_alti.cpp
+++ ground_station/src/fake_alti.cpp
@@ -1,29 +1,72 @@
 #include <ros/ros.h>
-#include <geometry_msgs/Pose.h>
+#include <asctec_hl_comm/mav_imu.h>
+#include <cmath>
 
-int main (int argc, char **argv)
-{
-  geometry_msgs::Pose pose;
-  double alt = 0, inc = 0;
-  bool incV = true;
-  ros::init (argc, argv, "fake_alti");
-  ros::NodeHandle n;
-  ros::Publisher chatter_pub = n.advertise < geometry_msgs::Pose > ("fake_alti", 1);
-  ros::Rate loop_rate (20);
-  while (ros::ok ())
-  {
-    alt += inc;
-    if (incV)
-      inc += 0.01;
-    else
-      inc -= 0.01;
-    if (inc > 2)
-      incV = false;
-    else if (inc <= -1)
-      incV = true;
-    pose.position.z = alt;
-    chatter_pub.publish (pose);
-    ros::spinOnce ();
-    loop_rate.sleep ();
-  }
+int main(int argc, char **argv) {
+	asctec_hl_comm::mav_imu imu;
+	ros::init(argc, argv, "fake_data");
+	ros::NodeHandle n;
+	ros::Publisher chatter_pub = n.advertise<asctec_hl_comm::mav_imu>("fake_data", 1);
+
+	int rate = 10; // Hz
+	// 20 Hz means the loop runs every 50 milliseconds
+	ros::Rate loop_rate(rate);
+
+	const double max_height = 1000.0; // metres
+	double alt = max_height/2.0;
+	double c = 0.5;
+	double inc = 0.001;
+	double prev_height = alt;
+	double delta_height;
+
+	double s = 0, s_prev = 0, v = 0, v_prev = 0;
+	double a = 10;
+	double t = 1.0/(double)rate;
+
+	double turning_t = std::sqrt(max_height/a);
+	double turning_v = a * turning_t;
+
+	//ROS_INFO_STREAM("t_t = " << turning_t << "\tt_v = " << turning_v);
+
+	while (ros::ok()) {
+		v = v_prev + a*t;
+		s = s_prev + v_prev * t + 0.5 * a * t*t;
+		v_prev = v;
+		s_prev = s;
+
+		if (v >= turning_v) {
+			a = -10.0;
+		}
+		else if (v <= -turning_v) {
+			a = 10.0;
+		}
+
+		//ROS_INFO_STREAM("v = " << v);
+		//ROS_INFO_STREAM("a = " << a);
+		//ROS_INFO_STREAM("s = " << s);
+		//ROS_INFO_STREAM("t = " << t);
+
+		/*
+		if (c > 1.0) {
+			c = 1.0;
+			inc = -0.001;
+		}
+		if (c < 0.0) {
+			c = 0.0;
+			inc = 0.001;
+		}
+		alt = c*max_height;
+		c += inc;
+		*/
+		alt = s;
+
+		delta_height = alt - prev_height;
+		prev_height = alt;
+
+		imu.height = alt * 1000.0;
+		imu.differential_height = delta_height * 1000.0;
+		chatter_pub.publish(imu);
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
 }
